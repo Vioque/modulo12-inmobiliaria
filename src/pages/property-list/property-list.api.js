ba1b07c7a038567e2d524b29b63584406f80fdbf
Vioque <jvioquea@hotmail.com>
2023-03-14 07:52:52 +0100

    Obtención de menú y parte de recuperación de los filtros.

--- src/pages/property-list/property-list.api.js
+++ src/pages/property-list/property-list.api.js
@@ -6,3 +6,17 @@ export const getPropertyList = () =>
   axios.get(url).then(response => {
     return response.data
   })
+
+const saleTypeListUrl = `${process.env.BASE_API_URL}/saleTypes`
+
+export const getSaleTypeList = () =>
+  axios.get(saleTypeListUrl).then(respose => {
+    return respose.data
+  })
+
+const provinceListUrl = `${process.env.BASE_API_URL}/provinces`
+
+export const getSprovinceList = () =>
+  axios.get(provinceListUrl).then(respose => {
+    return respose.data
+  })
--- src/pages/property-list/property-list.js
+++ src/pages/property-list/property-list.js
@@ -1,8 +1,64 @@
-import {getPropertyList} from "./property-list.api";
+import {getPropertyList, getSaleTypeList, getSprovinceList} from "./property-list.api";
 import {mapPropertyListFromApiToViewModel} from "./property-list.mappers";
-import {addPropertyRows} from "./property-list.helpers";
+import {addPropertyRows, setOptions} from "./property-list.helpers";
+import {bathrooomOptions, maxPriceOptions, minPriceOptions, roomOptions} from "./property-list.constants";
+import {onSubmitForm, onUpdateField} from "../../common/helpers";
 
-getPropertyList().then(propertyList => {
+
+Promise.all([
+  getPropertyList(),
+  getSaleTypeList(),
+  getSprovinceList(),
+]).then(
+  ([propertyList, saleTypeList, provinceList]) => {
+    loadPropertyList(propertyList)
+    setOptions(saleTypeList, 'select-sale-type', 'Tipo')
+    setOptions(provinceList, 'select-province', '¿Dónde?')
+    setOptions(roomOptions, 'select-room', 'Nº Habitaciones')
+    setOptions(bathrooomOptions, 'select-bathroom', 'Baños')
+    setOptions(minPriceOptions, 'select-min-price', 'Precio mínimo')
+    setOptions(maxPriceOptions, 'select-max-price', 'Precio máximo')
+  }
+)
+const loadPropertyList = propertyList => {
   const viewModelPropertyList = mapPropertyListFromApiToViewModel(propertyList)
   addPropertyRows(viewModelPropertyList)
+}
+
+
+let filter = {
+  saleTypeId: '',
+  provinceId: '',
+  minRooms: '',
+  minBathrooms: '',
+  minPrice: '',
+  maxPrice: ''
+}
+
+onUpdateField('select-sale-type', (event) => {
+  const value = event.target.value
+  filter = {
+    ...filter,
+    saleTypeId: value,
+  }
 })
+onUpdateField('select-province', (event) => {
+  const value = event.target.value
+  filter = {
+    ...filter,
+    provinceId: value,
+  }
+})
+
+onUpdateField('select-room', (event) => {
+  const value = event.target.value
+  filter = {
+    ...filter,
+    minRooms: value,
+  }
+})
+
+onSubmitForm('search-button', () => {
+  console.log({filter})
+})
+
